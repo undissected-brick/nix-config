4312f5af25eed88fddd748102620748cd7f1c63e
undissected-brick <undissectedbrick@tutamail.com>
2025-04-16 10:20:24 +0100

    added libsecret, and added seahorse to dbus packages. set up gnome-keyring as a systemd service

--- modules/system/secrets/default.nix
+++ modules/system/secrets/default.nix
@@ -1,3 +1,20 @@
-{
+# gnome-keyring setup from github.com/jordanisaacs/dotfiles
+{ pkgs, ... }: {
+	environment.systemPackages = [ pkgs.libsecret ];
 	services.gnome.gnome-keyring.enable = true;
+	services.dbus.packages = [ pkgs.seahorse ];
+
+	systemd.user.services.gnome-keyring = {
+		unitConfig = {
+			Description = "GNOME Keyring";
+			PartOf = [ "graphical-session-pre.target" ];
+		};
+
+		serviceConfig = {
+			ExecStart = "/run/wrappers/bin/gnome-keyring-daemon --start --foreground";
+			Restart = "on-abort";
+		};
+
+		wantedBy = [ "graphical-session-pre.target" ]; 
+	};
 }
