7ce63488e31862f84ff747ea57ca1ed49f6f0165
undissected-brick <undissectedbrick@tutamail.com>
2025-04-21 22:18:55 +0100

    fun new feature: an org.freedesktop.secrets interface that doesn't work, using pass-secret-service

--- modules/system/secrets/default.nix
+++ modules/system/secrets/default.nix
@@ -1,20 +1,31 @@
-# gnome-keyring setup from github.com/jordanisaacs/dotfiles
-{ pkgs, ... }: {
-	environment.systemPackages = [ pkgs.libsecret ];
-	services.gnome.gnome-keyring.enable = true;
-	services.dbus.packages = [ pkgs.seahorse ];
+{ pkgs, ... }:
 
-	systemd.user.services.gnome-keyring = {
-		unitConfig = {
-			Description = "GNOME Keyring";
-			PartOf = [ "graphical-session-pre.target" ];
-		};
+{
+  environment.systemPackages = with pkgs; [ 
+		pass-secret-service 
+		gnupg seahorse # GPG's CLI input always fails for me, so I need seahorse to generate a key
+		pass
+		libsecret
+	];
 
-		serviceConfig = {
-			ExecStart = "/run/wrappers/bin/gnome-keyring-daemon --start --foreground";
-			Restart = "on-abort";
-		};
+  services.dbus.packages = [ pkgs.pass-secret-service ];
 
-		wantedBy = [ "graphical-session-pre.target" ]; 
-	};
+  systemd.user.services.pass-secret-service = {
+    enable = true;
+    serviceConfig = {
+      ExecStart = "${pkgs.pass-secret-service}/bin/pass_secret_service";
+      Restart = "always";
+			Environment = "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/%U/bus";
+    };
+    wantedBy = [ "default.target" ];
+  };
+
+  # Needed for D-Bus activation
+  environment.sessionVariables = {
+    XDG_DATA_DIRS = [
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    ];
+		ELECTRON_EXTRA_LAUNCH_ARGS = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-secret-service";
+		NIXOS_OZONE_WL = "1";  # Critical for Wayland
+  };
 }
