74517d4804d8143aff4e8a7f369fd83504803310
undissected-brick <undissectedbrick@tutamail.com>
2025-05-04 13:42:39 +0100

    new not feature: didn't add epson print driver as it won't build. but maybe one day i can uncomment the code to add it

--- modules/system/system-basics/print.nix
+++ modules/system/system-basics/print.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }: {
+{ /*lib,*/ pkgs, ... }: {
+	/*
+	nixpkgs.config.allowUnfreePredicate = pkg:
+		builtins.elem (lib.getName pkg) [ "epson_201207w" ];
+	*/
+
 	# Enable printing with CUPS:
-	services.printing.enable = true;
+	services.printing = {
+		enable = true;
+		/*
+		drivers = [ 
+			pkgs.epson_201207w 
+		];
+		*/
+	};
 }
