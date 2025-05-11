863017afa06dd95f83551daf89376edd2409dc53
undissected-brick <undissectedbrick@tutamail.com>
2025-05-11 17:50:41 +0100

    new features: (neovim) line number is now relative, cursorline is highlighted, page scrolling begins when the cursor is 4 lines from the top or bottom of screen, autoindent now affect commented lines as it would any other

--- modules/system/system-basics/print.nix
+++ modules/system/system-basics/print.nix
@@ -1,16 +1,15 @@
-{ /*lib,*/ pkgs, ... }: {
-	/*
+{ lib, pkgs, ... }: {
+	
 	nixpkgs.config.allowUnfreePredicate = pkg:
 		builtins.elem (lib.getName pkg) [ "epson_201207w" ];
-	*/
+	
 
 	# Enable printing with CUPS:
 	services.printing = {
 		enable = true;
-		/*
+		
 		drivers = [ 
 			pkgs.epson_201207w 
 		];
-		*/
 	};
 }
