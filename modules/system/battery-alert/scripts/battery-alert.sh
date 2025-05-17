226157459db0b5851724cbd5c912b026b3be78b7
undissected-brick <undissectedbrick@tutamail.com>
2025-05-17 09:32:45 +0100

    modified the battery alert script so it only alerts if not charging

--- modules/system/battery-alert/scripts/battery-alert.sh
+++ modules/system/battery-alert/scripts/battery-alert.sh
@@ -1,6 +1,8 @@
 batteryStatus=$(cat /sys/class/power_supply/BAT*/capacity)
+chargeStatus=$(cat /sys/class/power_supply/BAT*/status)
 
-if [ $batteryStatus -le 15 ]
-then
-	notify-send "battery low!"
+if [ $((batteryStatus)) -le 15 ]; then
+  if [ "$chargeStatus" != "Charging" ]; then
+    notify-send "battery low!"
+  fi
 fi
---
+++ modules/system/battery-alert/scripts/temp.sh
@@ -0,0 +1,5 @@
+if [ 5 -eq 6 ]; then
+  echo bad at maths
+else
+  echo better at maths
+fi
