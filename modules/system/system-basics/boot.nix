eaa090cf8117007f65055265c84f26c356cbb705
undissected-brick <undissectedbrick@tutamail.com>
2025-05-30 10:50:57 +0100

    set kernelParam loglevel to 4

--- modules/system/system-basics/boot.nix
+++ modules/system/system-basics/boot.nix
@@ -2,6 +2,9 @@
 	boot = {
 		loader.systemd-boot.enable = true;
 		loader.efi.canTouchEfiVariables = true;
-		kernelParams = [ "console=tty2" ]; # Print startup messages to tty2
+		kernelParams = [ 
+			"console=tty2" # Print startup messages to tty2
+			"loglevel=4" # My actual goal is to stop anything writing to tuigreet's login field
+		];
 	};
 }
