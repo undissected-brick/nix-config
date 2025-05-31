e97e374fcda91be8fdbe6335df2654708711419a
undissected-brick <undissectedbrick@tutamail.com>
2025-05-31 13:43:12 +0100

    increased battery alert threshold to 25%

--- modules/system/battery-alert/scripts/battery-alert.sh
+++ modules/system/battery-alert/scripts/battery-alert.sh
@@ -1,7 +1,7 @@
 batteryStatus=$(cat /sys/class/power_supply/BAT*/capacity)
 chargeStatus=$(cat /sys/class/power_supply/BAT*/status)
 
-if [ $((batteryStatus)) -le 15 ]; then
+if [ $((batteryStatus)) -le 25 ]; then
   if [ "$chargeStatus" != "Charging" ]; then
     notify-send "battery low!"
   fi
--- modules/system/battery-alert/scripts/temp.sh
+++
@@ -1,5 +0,0 @@
-if [ 5 -eq 6 ]; then
-  echo bad at maths
-else
-  echo better at maths
-fi
