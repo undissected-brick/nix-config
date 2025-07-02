881d32153cb70a7ed8b1351969f1ac9621054b5c
undissected-brick <undissectedbrick@tutamail.com>
2025-07-02 16:59:01 +0100

    updated yazi config to use the newer mgr

--- home-modules/yazi/default.nix
+++ home-modules/yazi/default.nix
@@ -38,7 +38,7 @@ in {
 		'';
 
     keymap = {
-      manager.prepend_keymap = [
+      mgr.prepend_keymap = [
         {
           on  = "y";
           run = [ ''shell -- for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list'' "yank" ];
