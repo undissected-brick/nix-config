37648a3c61f7b2a8dbc88a6170841ac3bf05ce09
undissected-brick <undissectedbrick@tutamail.com>
2025-07-24 14:59:43 +0100

    adjusted conform-nvim's usage of ocamlformat

--- modules/nvf/config/languages/ocaml.nix
+++ modules/nvf/config/languages/ocaml.nix
@@ -5,7 +5,14 @@
     };
     formatter.conform-nvim = {
       enable = true;
-      setupOpts.formatters_by_ft.ocaml = "ocamlformat";
+      setupOpts.formatters_by_ft.ocaml = { 
+				command = "ocamlformat";
+				arguments = [
+					"--if-then-else" "vertical"
+					"--break-cases" "fit-or-vertical"
+					"--type decl" "sparse"
+				];
+			};
     };
   };
 }
