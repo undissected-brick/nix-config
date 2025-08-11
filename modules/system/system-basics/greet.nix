80617be7dbb94d2f14eb7c5ead0da7c90636de35
undissected-brick <undissectedbrick@tutamail.com>
2025-08-11 09:03:44 +0100

    greetd.tuigreet was renamed to tuigreet

--- modules/system/system-basics/greet.nix
+++ modules/system/system-basics/greet.nix
@@ -2,7 +2,7 @@
 	services.greetd = {
 		enable = true;
 		settings.default_session = {
-			command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd Hyprland";
+			command = "${pkgs.tuigreet}/bin/tuigreet --remember --time --cmd Hyprland";
 			user = "greeter";
 		};
 	};
