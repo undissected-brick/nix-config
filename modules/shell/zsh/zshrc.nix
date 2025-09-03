104eccb97a1e78699b13751d542f2829917d69d8
undissected-brick <undissectedbrick@tutamail.com>
2025-09-03 11:33:53 +0100

    fixed a typo and added opam support to .zshrc

--- modules/shell/zsh/zshrc.nix
+++ modules/shell/zsh/zshrc.nix
@@ -1,3 +1,6 @@
 {
-	programs.zsh.shellInit = "eval \"$(direnv shell zsh\"";
+	programs.zsh.shellInit = ''
+		eval "$(direnv shell zsh)"
+		[[ ! -r '/home/joseph/.opam/opam-init/init.zsh' ]] || source '/home/joseph/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+	'';
 }
